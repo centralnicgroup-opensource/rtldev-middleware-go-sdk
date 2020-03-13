80693d046a1ec2d0a9f3e8cb94ee40c3eec2af28
Kai Schwarz <kschwarz@hexonet.net>
2020-03-13 15:59:39 +0100

    ci(travis): review code validation & test

--- scripts/test-go.sh
+++ scripts/test-go.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
-
-set -euo pipefail
-
 echo
 echo "==> Running automated tests <=="
-go test -coverprofile=coverage.out ./apiclient ./column ./record ./response ./responseparser ./responsetemplate ./responsetemplatemanager ./socketconfig
+go test -v -race -coverprofile=coverage.out ./apiclient ./column ./record ./response ./responseparser ./responsetemplate ./responsetemplatemanager ./socketconfig
 go tool cover -html=coverage.out -o coverage.html
-cd .. || exit
-exit
